b9fe8db51bf710979d3449e6ee6f317aeaaaa050
Rafay A. Abedi <rafay.abedi@gmail.com>
2023-04-05 17:06:53 +0500

    feat(jsx): antd Charts added. Dummy json loaded.

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,6 +1,69 @@
-function Chart() {
+import { Line } from '@ant-design/charts';
+import { useEffect, useState } from 'react';
+
+const Chart = () => {
+    const [conversionRateData, setConversionRateData] = useState([])
+    const getData = () => {
+        fetch('FixerDummyResponse.json',
+            {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+            }
+        )
+        .then( resp => resp.json() )
+        .then(response => {
+            console.log('response', response.rates);
+            let DataArr = [];
+            for (const key in response.rates) {
+                if (Object.hasOwnProperty.call(response.rates, key)) {
+                    const rate = response.rates[key];
+                    DataArr.push({date: key, conversionValue: rate['PKR']})
+                }
+            }
+            setConversionRateData(DataArr);
+            console.log('setConversionRateData', conversionRateData);
+
+        });
+      }
+      useEffect(()=>{
+        getData()
+      },[])
+    const data = [
+        { year: '1991', value: 3 },
+        { year: '1992', value: 4 },
+        { year: '1993', value: 3.5 },
+        { year: '1994', value: 5 },
+        { year: '1995', value: 4.9 },
+        { year: '1996', value: 6 },
+        { year: '1997', value: 7 },
+        { year: '1998', value: 9 },
+        { year: '1999', value: 13 },
+      ];
+    
+    const config = {
+        data,
+        width: 800,
+        height: 400,
+        autoFit: false,
+        xField: 'year',
+        yField: 'value',
+        point: {
+          size: 5,
+          shape: 'point',
+        },
+        label: {
+          style: {
+            fill: '#aaa',
+          },
+        },
+      };
     return (
-        <h1>Currency history chart</h1>
+        <div>
+            <h1>Currency history chart</h1>
+            { conversionRateData.length > 0 && <Line {...config} /> }
+        </div>
     );
 }
 
