568155581230032e1a11a70e7b1457d8501d0148
Rafay A. Abedi <rafay.abedi@gmail.com>
2023-04-05 15:49:12 +0500

    feat(jsx): currency api plugin used get the historical data of excahnge rates between USD and PKR

---
+++ src/components/currency-converter/currency-api-plugin.jsx
@@ -0,0 +1,34 @@
+import { Button } from 'antd';
+
+import CurrencyAPI from '@everapi/currencyapi-js';
+
+const CurrencyApiPlugin = () => {
+    const baseCurrency = "PKR";
+    const targetCurrency = "USD";
+    const historyDate = "2023-03-04"; //YYYY-MM-DD
+
+    const apiKey = "MGYFiZAyqNuKQZvW6594jT18YyiuIAIcFBxHqZS4";
+
+    const currencyApi = new CurrencyAPI(apiKey);
+
+    const convertHandler = () => {
+        currencyApi.range({
+            datetime_start: historyDate,
+            datetime_end: "2023-04-04",
+            base_currency: baseCurrency,
+            currencies: targetCurrency
+        }).then(response => {
+            console.log('response', response);
+        }).catch(err => {
+            console.log('err', err)
+        });
+    };
+
+    return (
+        <>
+            <Button type="primary" onClick={convertHandler}>Convert Currencyapi</Button>
+        </>
+    );
+}
+
+export default CurrencyApiPlugin;
